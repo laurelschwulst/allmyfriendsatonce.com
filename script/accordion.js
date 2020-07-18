eaf94b44451d896eab1252c5b5af439e67fbdd96
Laurel Schwulst <laurel.schwulst@gmail.com>
2020-07-17 21:44:35 -0400

    essential only

--- script/accordion.js
+++
@@ -1,6 +0,0 @@
-$(window).load(function(){
-	$(document).on('click', '.accordion-header', function(e){
-        e.preventDefault();
-        $(this).closest('li').find('.accordion-body').slideToggle();
-    });
-});
